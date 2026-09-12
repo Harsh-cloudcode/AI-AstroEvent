148a76d1029609bd1c81ac18ea3e92f6cbe4c6b2
Harshal Deshmukh <136334910+Harsh-cloudcode@users.noreply.github.com>
2026-09-12 19:56:49 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -21,22 +21,17 @@ app = FastAPI(
 )
 
 
-# --------------------------------------------------
-# CORS
-# --------------------------------------------------
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000"
-        "https://ai-astroevent.onrender.com",
+        "http://127.0.0.1:3000",
+        "https://ai-astro-event.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 # --------------------------------------------------
 # Basic Routes
