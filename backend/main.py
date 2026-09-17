dd09c13c20f97008e6c2b46ac1a16597d8550f95
Harshal Deshmukh <136334910+Harsh-cloudcode@users.noreply.github.com>
2026-09-17 09:12:56 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -27,6 +27,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://ai-astro-event.vercel.app",
+        "https://ai-astro-event-84dq60j1v-harshaldeshmukh386-2972s-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
